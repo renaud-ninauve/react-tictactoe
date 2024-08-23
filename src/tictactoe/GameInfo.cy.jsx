7815331dc2e6f0941397f8ba7ad753a76d57d760
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-08-23 11:13:13 +0200

    GameInfos and tests

---
+++ src/tictactoe/GameInfo.cy.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { GameInfo } from './GameInfo'
+
+describe('<GameInfo />', () => {
+  const GAME_START_INFO = 'Go to game start';
+
+  it('beginning of the game', () => {
+    const moves = [0];
+    cy.mount(<GameInfo moves={moves}/>);
+
+    cy.get('li')
+      .should('have.length', 1);
+    
+    cy.get('li button').should('have.text', GAME_START_INFO);
+  }),
+
+  it('later in the game', () => {
+    const moves = [0, 1, 2, 3];
+    const currentMove = 2;
+    const jumpToSpy = cy.spy().as('jumpToSpy');
+
+    cy.mount(<GameInfo moves={moves} currentMove={currentMove} jumpTo={jumpToSpy} />);
+
+    const expectedInfos = [
+      GAME_START_INFO,
+      'move 1',
+      'You are at move 2',
+      'move 3'
+    ];
+
+    cy.get('li')
+      .should('have.length', expectedInfos.length);
+
+    cy.get('li').each((el, index) => {
+      cy.wrap(el)
+        .contains(expectedInfos[index])
+        .should('exist');
+    });
+
+    // buttons for jump to another move
+    cy.get('li').each((el, index) => {
+      if (index === 2) {
+        return;
+      }
+      cy.wrap(el)
+        .find('button')
+        .should('exist');
+    });
+
+    // no buttons for current move
+    cy.get('li')
+      .eq(2)
+      .find('button')
+      .should('not.exist');
+      
+    // click -> jumpTo
+    cy.get('li').eq(1).find('button').click();
+    cy.get('@jumpToSpy').should('have.been.calledWith', 1);
+  })
+})
---
+++ src/tictactoe/GameInfo.jsx
@@ -0,0 +1,11 @@
+export function GameInfo({moves, currentMove, jumpTo}) {
+    const infos = moves.map((moveSquares, moveNumber) => {   
+        if (moveNumber === currentMove) {
+            return (<li key={moveNumber}>You are at move {moveNumber}</li>);
+        }     
+        const description = moveNumber === 0 ? 'Go to game start':`move ${moveNumber}`;
+        return (<li key={moveNumber}><button onClick={() => jumpTo(moveNumber)}>{description}</button></li>);
+    });
+
+    return (<ul>{infos}</ul>);
+}
